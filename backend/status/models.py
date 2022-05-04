53ea6925881ed101d4b5e1c6846d9526bc8b05a7
Emmanuel <e.m.social35@gmail.com>
2022-05-04 15:37:03 -0500

    change user model to auth user + change user so that branch, grade, and status are not foreign keys to the user but instead choices

--- backend/status/models.py
+++ backend/status/models.py
@@ -3,4 +3,4 @@ from django.db import models
 # Create your models here.
 
 class MilitaryStatus(models.Models):
-    status:models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
